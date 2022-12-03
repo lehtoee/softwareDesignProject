db1e3cb94cc7541c2ed8c0e95311197d3bb9e023
Veikka Peltomäki <veikka.peltomaki@tuni.fi>
2022-12-03 13:48:50 +0200

    Added todo comments to main

--- code/vnes-app/main.cpp
+++ code/vnes-app/main.cpp
@@ -11,3 +11,35 @@ int main(int argc, char *argv[])
     view->show();
     return a.exec();
 }
+
+/* TODO
+ * Veikka:
+ * - Time funktio observation vs forecast
+ * - Connecti controllerin ja networkhandlerin väliin jotta saadaan data sinne
+- The user must be able to request calculations and visualization on average daily
+temperature at certain location in certain month
+- The user must be able to request calculations and visualization on maximum and
+minimum daily temperature at certain location in certain month
+ *
+ *
+ * Niklas:
+ * - Monthly averages weather datan datatyypiks, disablee timeline valinnan (aina 30 pvä)
+ * - avg temp / day, max temp / day, min temp / day
+ *
+ * Eemil:
+ * - Connecti controllerin ja networkhandlerin väliin jotta saadaan data sinne
+ * - Parserin siirto
+ *
+ * Saha?
+ *
+ * Yleistä:
+ * - Frontin ja backendin yhdistäminen
+ * - Datan tallennus / kuvaajan tallennus
+ *
+ *
+- Combine weather information (from FMI) and road maintenance information from a
+selected location and time interval into one window.
+- Combine weather information (from FMI) and road condition forecast information from
+a selected location and time interval into one window.
+ *
+ * */
